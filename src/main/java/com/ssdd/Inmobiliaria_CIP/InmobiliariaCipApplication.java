1f647153ef516cd86a722a66cf15b412eb251340
Torregrosa Mateo <ivan.torregrosa@ormazabal.com>
2024-03-06 12:29:36 +0100

    Primer commit

---
+++ src/main/java/com/ssdd/Inmobiliaria_CIP/InmobiliariaCipApplication.java
@@ -0,0 +1,13 @@
+package com.ssdd.Inmobiliaria_CIP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmobiliariaCipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmobiliariaCipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssdd/Inmobiliaria_CIP/InmobiliariaCipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssdd.Inmobiliaria_CIP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmobiliariaCipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
