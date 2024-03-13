911077c64d3fc04443d6128a2b65a867e85243e9
Torregallar <torregallar2uni@gmail.com>
2024-03-13 14:23:58 +0100

    Property controller almost done

--- src/main/java/com/ssdd/Inmobiliaria_CIP/controllers/PropertyController.java
+++ src/main/java/com/ssdd/Inmobiliaria_CIP/controllers/PropertyController.java
@@ -5,24 +5,23 @@ import com.ssdd.Inmobiliaria_CIP.services.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Field;
 
 @Controller
-@RequestMapping("/properties")
 public class PropertyController {
 
     @Autowired
     PropertyService propertyService;
 
-    @GetMapping
+    @GetMapping("/properties")
     public String getProperties (Model model){
         model.addAttribute("properties", propertyService.getProperties());
         return "properties";
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/property/{id}")
     public String getProperty (Model model, @PathVariable int id) {
         Property property = propertyService.getProperty(id);
         if (property != null) {
@@ -31,4 +30,29 @@ public class PropertyController {
         }
         return "redirect/properties";
     }
+
+    @PostMapping("/properties/createProperty")
+    public String createProperty (Property property, Model model) {
+        propertyService.createProperty(property);;
+        return "redirect:/properties";
+    }
+
+    @GetMapping("/property/deleteProperty/{id}")
+    public String deleteProperty(@PathVariable int id) {
+        propertyService.deleteProperty(id);
+        return "redirect:/properties";
+    }
+
+    @GetMapping("/property/modify/{id}")
+    public String modifyProperty(@PathVariable int id, Model model) {
+        model.addAttribute("property", propertyService.getProperty(id));
+        return "/modify-property";
+    }
+
+    @PostMapping("/property/modify/{id}")
+    public String modifyProperty(@PathVariable int id, Property property) {
+        propertyService.updateProperty(id, property);
+        return "redirect:/property/{id}";
+    }
+
 }
