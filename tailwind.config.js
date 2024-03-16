b66bd4f4ca30d1d4332d9d5b1b3994e6119a8819
Torregallar <torregallar2uni@gmail.com>
2024-03-16 16:25:38 +0100

    First try with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./src/**/*.{js}"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
