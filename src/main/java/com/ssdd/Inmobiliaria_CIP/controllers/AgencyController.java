edf3a49f2f87594c1410410cd9a613226f45d4a1
Carlos <cbolivar2001@gmail.com>
2024-03-17 11:07:42 +0100

    update

--- src/main/java/com/ssdd/Inmobiliaria_CIP/controllers/AgencyController.java
+++ src/main/java/com/ssdd/Inmobiliaria_CIP/controllers/AgencyController.java
@@ -1,2 +1,58 @@
-package com.ssdd.Inmobiliaria_CIP.controllers;public class AgencyController {
+package com.ssdd.Inmobiliaria_CIP.controllers;
+
+import com.ssdd.Inmobiliaria_CIP.entities.Agency;
+import com.ssdd.Inmobiliaria_CIP.entities.Property;
+import com.ssdd.Inmobiliaria_CIP.services.AgencyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AgencyController {
+
+    @Autowired
+    AgencyService AgencyService;
+
+    @GetMapping("/agencies")
+    public String getAgencies (Model model){
+        model.addAttribute("agencies", AgencyService.getAgencies());
+        return "agency";
+    }
+
+    @GetMapping("/agency/{id}")
+    public String getAgency (Model model, @PathVariable int id) {
+        Agency agency = AgencyService.getAgency(id);
+        if (agency != null) {
+            model.addAttribute("agency", agency);
+            return "agency-details";
+        }
+        return "redirect:/agency";
+    }
+
+    @PostMapping("/agencies/createAgency")
+    public String createProperty (Agency agency) {
+        AgencyService.createAgency(agency);
+        return "redirect:/agency";
+    }
+
+    @GetMapping("/agency/deleteAgency/{id}")
+    public String deleteAgency(@PathVariable int id) {
+        AgencyService.deleteAgency(id);
+        return "redirect:/agency";
+    }
+
+    @GetMapping("/agency/modify/{id}")
+    public String modifyAgency(@PathVariable int id, Model model) {
+        model.addAttribute("agency", AgencyService.getAgency(id));
+        return "/modify-agency";
+    }
+
+    @PostMapping("/agency/modify/{id}")
+    public String modifyAgency(@PathVariable int id, Agency agency) {
+        AgencyService.updateAgency(id, agency);
+        return "redirect:/agency/{id}";
+    }
 }
--- src/main/java/com/ssdd/Inmobiliaria_CIP/entities/Agency.java
+++ src/main/java/com/ssdd/Inmobiliaria_CIP/entities/Agency.java
@@ -1,2 +1,50 @@
-package com.ssdd.Inmobiliaria_CIP.entities;public class Agency {
+package com.ssdd.Inmobiliaria_CIP.entities;
+
+public class Agency {
+    private int id;
+    private String name;
+    private long phone;
+    private String email;
+
+    public Agency() {
+    }
+
+    public Agency(int id, String name, long phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
