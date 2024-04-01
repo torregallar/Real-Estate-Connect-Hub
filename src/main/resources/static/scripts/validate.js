38902dd2e7e1eb2a38ba77d4b08f0098c875ffda
Torregallar <torregallar2uni@gmail.com>
2024-04-01 20:32:24 +0200

    Validaciones de campos formulario hecho

--- src/main/resources/static/scripts/validate.js
+++ src/main/resources/static/scripts/validate.js
@@ -9,10 +9,10 @@ button.addEventListener('click', function(event) { // Comprobación campos vací
 
     var fields = form.querySelectorAll('input');
     var numberFields = form.querySelectorAll('input[type="number"]')
-    var email = document.getElementById("email")
-    var fieldsEmpty = false;
-    var intOrLong = []
-    var doubles = []
+    var telephone = form.querySelectorAll('input[type="tel"]')
+    var email = document.querySelectorAll('input[type="email"]')
+    var isValidTel = true
+    var fieldsEmpty = false
     var isValidNumber = true
     var isEmail = true
 
@@ -22,36 +22,41 @@ button.addEventListener('click', function(event) { // Comprobación campos vací
             field.style.borderColor = 'red';
         }
     })
+    if (email.length>0) {
+        isEmail = email[0].value.trim().includes('@')
+    }
 
-    numberFields.forEach(function (field) {
-        if (field.id === "sqMetres" || field.id === "price") {
-            doubles.push(field);
-        } else {
-            intOrLong.push(field);
-        }
-    });
+    if (telephone.length>0) {
+        isValidTel = /^\d{9}$/.test(telephone[0].value.trim())
+    }
 
-    intOrLong.forEach(function (field) {
-        isValidNumber = /^\d+$/.test(field.value.trim());
-    })
+    if (numberFields.length>0) {
+        numberFields.forEach(function (field) {
+            if (!(field.id === "sqMetres" || field.id === "price")) {
+                if (!(/^\d*$/.test(field.value.trim()))) {
+                    isValidNumber = false
+                }
+            }
+        })
+    }
 
-    doubles.forEach(function (field) {
-        isValidNumber = /^\d*\.?\d*$/.test(field.value.trim());
-    })
-
-    isEmail = email.value.trim().includes('@')
 
     if (fieldsEmpty) {
         Swal.fire({
             title: "WARNING",
             text: "Fields should not be empty"
         });
-    } else if (isValidNumber) {
+    } else if (!isValidTel) {
         Swal.fire({
             title: "WARNING",
-            text: 'Use ONLY NUMBERS in number fields, and use "." to split a real number'
+            text: 'Telephone should be 9 numbers'
         });
-    } else if (isEmail) {
+    } else if (!isValidNumber) {
+        Swal.fire({
+            title: "WARNING",
+            text: 'Integer inputs should cointain only numbers'
+        });
+    } else if (!isEmail) {
         Swal.fire({
             title: "WARNING",
             text: 'Use "@" in your EMAIL'
