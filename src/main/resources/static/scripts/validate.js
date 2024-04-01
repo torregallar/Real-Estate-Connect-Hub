b25dd45d67f2d5300a7ef482827a350343941e98
Torregallar <torregallar2uni@gmail.com>
2024-04-01 18:55:59 +0200

    Sigo con comprobaciones del formulario

--- src/main/resources/static/scripts/validate.js
+++ src/main/resources/static/scripts/validate.js
@@ -1,14 +1,20 @@
 
 var button = document.getElementById('createButton');
+var form = document.getElementById('form');
 
-button.addEventListener('click', function(event) {
 
-    var form = document.getElementById('form');
+button.addEventListener('click', function(event) { // Comprobación campos vacíos
 
     event.preventDefault();
 
     var fields = form.querySelectorAll('input');
+    var numberFields = form.querySelectorAll('input[type="number"]')
+    var email = document.getElementById("email")
     var fieldsEmpty = false;
+    var intOrLong = []
+    var doubles = []
+    var isValidNumber = true
+    var isEmail = true
 
     fields.forEach(function (field) {
         if (field.value.trim() === '') {
@@ -17,16 +23,40 @@ button.addEventListener('click', function(event) {
         }
     })
 
-    if (!fieldsEmpty) {
-        form.submit();
+    numberFields.forEach(function (field) {
+        if (field.id === "sqMetres" || field.id === "price") {
+            doubles.push(field);
+        } else {
+            intOrLong.push(field);
+        }
+    });
+
+    intOrLong.forEach(function (field) {
+        isValidNumber = /^\d+$/.test(field.value.trim());
+    })
+
+    doubles.forEach(function (field) {
+        isValidNumber = /^\d*\.?\d*$/.test(field.value.trim());
+    })
+
+    isEmail = email.value.trim().includes('@')
+
+    if (fieldsEmpty) {
+        Swal.fire({
+            title: "WARNING",
+            text: "Fields should not be empty"
+        });
+    } else if (isValidNumber) {
+        Swal.fire({
+            title: "WARNING",
+            text: 'Use ONLY NUMBERS in number fields, and use "." to split a real number'
+        });
+    } else if (isEmail) {
+        Swal.fire({
+            title: "WARNING",
+            text: 'Use "@" in your EMAIL'
+        });
     } else {
-        alert("All fields should be filled");
+        form.submit();
     }
 })
-
-
-
-
-
-
-
