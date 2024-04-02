2906c3944be16a0122d59985c2f4379037ca3537
Torregallar <torregallar2uni@gmail.com>
2024-04-02 16:40:24 +0200

    delete.js fixed, and forms validation complete

--- src/main/resources/static/scripts/delete.js
+++ src/main/resources/static/scripts/delete.js
@@ -3,8 +3,15 @@ document.querySelector('[id^="deleteButton_"]').addEventListener('click', functi
 
     var message = confirm("Do you want to delete the entity?")
     var id = this.id.replace("deleteButton_", "");
+    var buttonClass = this.dataset.entity;
     if (message) {
-        window.location.href="/agency/deleteAgency/" + id
+        if (buttonClass === "agency") {
+            window.location.href = "/agency/deleteAgency/" + id
+        } else if (buttonClass === "owner") {
+            window.location.href = "/owner/deleteOwner/" + id
+        } else if (buttonClass === "property") {
+            window.location.href = "/property/deleteProperty/" + id
+        }
     } else {
         alert("Deletion cancelled");
     }
